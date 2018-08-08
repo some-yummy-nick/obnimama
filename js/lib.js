78b93e5f578b8e74d9d997431c2eb7bf31c52bdc
yeffasol <yeffasol@mail.com>
2018-08-08 09:56:52 +0300

    Add modaal plugin

--- js/lib.js
+++ js/lib.js
@@ -19,17 +19,6 @@ function addPhoneMask(elements) {
 	});
 }
 
-function scrollLinks(links) {
-  links.click(function () {
-	  if ($(this).attr("href") == "" || $(this).attr("href") == "#" || $(this).attr("href") == "javascript:void(0)") {
-      return false;
-    }
-    var elementClick = $(this).attr("href");
-    // $('html,body').stop().animate({scrollTop: $(elementClick).offset().top}, 1000);
-    return false;
-  });
-}
-
 function addVoidForLinks(links) {
   $.each(links, function () {
     if ($(this).attr("href") == "" || $(this).attr("href") == "#") {
--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
-var globalPopup;
-
 $(document).ready(function () {
-	globalPopup = new Popup();
 	addVoidForLinks($("a"));
 	setEqualHeight($(".row>.col"));
 	hamburger('js-hamburger', "js-menu");
 	addPhoneMask('.js-phone');// https://unmanner.github.io/imaskjs/guide.html
-
-	$('.js-modal-link').click(function () {
-		$.post("ajax/modal.html", function (response) {
-			globalPopup.html(response, function () {
-
-			}).show();
-		});
-		return false;
-	})
+	$('.modaal-ajax').modaal({
+		type: 'ajax'
+	});
 });
