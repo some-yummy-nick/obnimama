898bbeddb7efd5fca43488a84524d88bb512372c
yeffasol <yeffasol@mail.com>
2018-08-09 17:37:54 +0300

    Add first commit

--- js/lib.js
+++ js/lib.js
@@ -1,15 +1,3 @@
-function setEqualHeight(columns) {
-  var tallestcolumn = 0;
-  columns.each(function () {
-    var currentHeight = $(this).height();
-    console.log(currentHeight);
-    if (currentHeight > tallestcolumn) {
-      tallestcolumn = currentHeight;
-    }
-  });
-  columns.height(tallestcolumn);
-}
-
 function addPhoneMask(elements) {
 	var phones = document.querySelectorAll(elements);
 	Array.prototype.forEach.call(phones, function(child) {
@@ -27,22 +15,121 @@ function addVoidForLinks(links) {
   });
 }
 
-//a11y hamburger https://foxland.fi/simple-accessible-svg-menu-hamburger-animation
-function hamburger(element, menu) {
-  var button = document.getElementById(element),
-    menu = document.getElementById(menu);
-  button.onclick = function () {
-    // Toggle class "opened". Set also aria-expanded to true or false.
-    if (-1 !== button.className.indexOf('opened')) {
-      button.className = button.className.replace(' opened', '');
-      button.setAttribute('aria-expanded', 'false');
-      menu.className = menu.className.replace(' active', '');
-      menu.setAttribute('aria-expanded', 'false');
-    } else {
-      button.className += ' opened';
-      button.setAttribute('aria-expanded', 'true');
-      menu.className += ' active';
-      menu.setAttribute('aria-expanded', 'true');
-    }
-  };
-}
+//start accordion
+(function ($) {
+	var methods = {
+		init: function (options) {
+			var defaults = {
+				close: false
+			};
+			var keys = {
+				down: 40,
+				up: 38
+			};
+			return this.each(function () {
+				var settings = $.extend(defaults, options);
+				var self = $(this);
+				var that = this;
+				var button = self.find(".js-accordion__button");
+				var close = self.attr("data-close");
+				var label = self.attr("aria-label");
+
+				self.on("keydown", function (e) {
+					var focused = $(":focus");
+
+					if (e.keyCode == keys.down) {
+						if (focused.parent().next()) {
+							focused
+								.parent()
+								.next()
+								.find(".js-accordion__button")
+								.focus();
+						}
+					} else if (e.keyCode == keys.up) {
+						if (focused.parent().prev()) {
+							focused
+								.parent()
+								.prev()
+								.find(".js-accordion__button")
+								.focus();
+						}
+					}
+				});
+
+				button.each(function ( index) {
+					var child = $(this);
+					var block = this.parentNode;
+					var content = $(this)[0].nextElementSibling;
+					var selected = child.attr("aria-selected");
+					child.attr("aria-expanded", selected);
+					child.attr("id", label + "-tab-" + index);
+					child.attr("aria-controls", label + "-panel-" + index);
+					content.setAttribute("aria-labelledby",label + "-tab-" + index );
+					content.setAttribute("id",label + "-panel-" + index );
+					content.setAttribute("aria-hidden", true);
+
+					function activeBlocks() {
+						if (child.attr("aria-selected") == "true") {
+							button.each(function () {
+								$(this).attr("aria-selected", false);
+							})
+							child.attr("aria-selected", true);
+							block.classList.add("active");
+							content.setAttribute("aria-hidden", false);
+							content.style.maxHeight = content.scrollHeight + "px";
+						}
+					}
+
+					activeBlocks();
+
+					child.on("click", function () {
+						button.each(function () {
+							$(this).attr("aria-selected", "false");
+						});
+						child.attr("aria-selected", "true");
+
+						if (settings.close || close) {
+							var otherBlocks = that.querySelectorAll(".js-accordion__block");
+							var otherContent = that.querySelectorAll(".js-accordion__content");
+
+							for (let i = 0; i < otherContent.length; i++) {
+								otherContent[i].style.maxHeight = null;
+							}
+						}
+						if (block.classList.contains("active")) {
+							block.classList.remove("active");
+							content.setAttribute("aria-hidden", "true");
+							child.attr("aria-expanded", "false");
+							content.style.maxHeight = null;
+						} else {
+							if (settings.close || close) {
+								for (let i = 0; i < otherBlocks.length; i++) {
+									otherBlocks[i].classList.remove("active");
+								}
+							}
+							block.classList.add("active");
+							content.setAttribute("aria-hidden", "false");
+							child.attr("aria-expanded", "true");
+							content.style.maxHeight = content.scrollHeight + "px";
+						}
+					});
+				});
+			});
+		}
+	};
+
+	$.fn.accordion = function (action) {
+		if (methods[action]) {
+			return methods[action].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof action === "object" || !action) {
+			return methods.init.apply(this, arguments);
+		} else {
+			console.info("Action " + "'" + action + "'" + " not found in this plugin");
+		}
+	};
+})(jQuery);
+//end accordion
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,9 +1,168 @@
 $(document).ready(function () {
 	addVoidForLinks($("a"));
-	setEqualHeight($(".row>.col"));
-	hamburger('js-hamburger', "js-menu");
 	addPhoneMask('.js-phone');// https://unmanner.github.io/imaskjs/guide.html
-	$('.modaal-ajax').modaal({
-		type: 'ajax'
+	$(".js-accordion").accordion();
+
+	$('.js-modaal-question').modaal({
+		type: 'ajax',
+		custom_class: "callback-popup"
 	});
+
+	$('.js-product-modal').modaal({
+		type: 'ajax',
+		custom_class: "product-popup"
+	});
+
+	baron({
+		root: '.js-baron',
+		scroller: '.js-scroll',
+		bar: '.js-scrollbar',
+		scrollingCls: '_scrolling',
+		draggingCls: '_dragging'
+	});
+
+	$('.js-select-basket').select2({
+		theme: "basket",
+		minimumResultsForSearch: -1
+	});
+
+	$('.js-select-hour').select2({
+		theme: "hour",
+		minimumResultsForSearch: -1
+	});
+
+	$('.js-select-minute').select2({
+		theme: "minute",
+		minimumResultsForSearch: -1
+	});
+
+	(function () {
+		var doc = document,
+			basketButtons = doc.querySelectorAll(".js-basket-button"),
+			basketClose = doc.querySelector(".js-basket-close"),
+			basketInitial = doc.querySelector(".js-basket-initial"),
+			basketStuff = doc.querySelectorAll(".js-basket-stuff"),
+			basketAdmire = doc.querySelector(".js-admire"),
+			basketFields = doc.querySelectorAll(".js-field");
+
+		basketAdmire.addEventListener("change", function () {
+			var basketButton = $(basketAdmire).parents(".js-basket-block").find(".js-basket-button")[0];
+			basketButton.disabled = !basketAdmire.checked;
+		});
+
+		Array.prototype.forEach.call(basketButtons, function (basketButton) {
+			basketButton.addEventListener("click", function () {
+				var basketBlock = $(basketButton).parents(".js-basket-block")[0];
+				var basketFields = basketBlock.querySelectorAll(".js-field");
+				if (basketFields.length) {
+					Array.prototype.forEach.call(basketFields, function (basketField) {
+						if (basketField.value.trim() !== "") {
+							basketBlock.classList.add("hidden");
+							basketBlock.nextElementSibling.classList.toggle("hidden");
+
+						}
+						else {
+							if(!$(basketField).parent().find(".form__error").length){
+								basketField.insertAdjacentHTML('afterend', '<p class="form__error">' + "Пожалуста заполните поле" + '</p>')
+							}
+						}
+					});
+				} else {
+					basketBlock.classList.add("hidden");
+					basketBlock.nextElementSibling.classList.toggle("hidden");
+				}
+
+
+			})
+		});
+
+		Array.prototype.forEach.call(basketFields, function (basketField) {
+			basketField.addEventListener("input", function () {
+
+				if (basketField.nextElementSibling) {
+					basketField.nextElementSibling.classList.add("hidden");
+				}
+			})
+		});
+
+		basketClose.addEventListener("click", function () {
+			basketInitial.classList.remove("hidden");
+			var basketBlock = $(basketClose).parents(".js-basket-block")[0];
+			basketBlock.classList.add("hidden");
+			Array.prototype.forEach.call(basketStuff, function (child) {
+				child.innerHTML = 0;
+			});
+		})
+	})();
+
+	(function () {
+		var doc = document,
+			products = doc.querySelectorAll(".js-product"),
+			basketInitial = doc.querySelector(".js-basket-initial"),
+			basketFirst = doc.querySelector(".js-basket-first"),
+			basketItems = doc.querySelector(".js-basket-items"),
+			basketCount = doc.querySelector(".js-basket-count");
+
+		Array.prototype.forEach.call(products, function (product) {
+
+			var amount = product.querySelector(".js-amount"),
+				plus = product.querySelector(".js-basket-plus"),
+				minus = product.querySelector(".js-basket-minus"),
+				stuff = product.querySelector(".js-basket-stuff"),
+				price = product.querySelector(".js-basket-price"),
+				priceInitial = price.innerHTML,
+				title = product.querySelector(".js-product-title"),
+				listItem = doc.createElement("li"),
+				listItemName = doc.createElement("div"),
+				listItemAmount = doc.createElement("div");
+
+			listItem.classList.add("basket__item");
+			listItemName.classList.add("basket__name");
+			listItemAmount.classList.add("basket__amount");
+
+			listItem.appendChild(listItemName);
+			listItem.appendChild(listItemAmount);
+
+
+			plus.addEventListener("click", function () {
+				stuff.innerHTML++;
+				basketInitial.classList.add("hidden");
+				basketFirst.classList.remove("hidden");
+				listItemName.innerHTML = title.innerHTML;
+				$(basketItems).parents(".basket__block").removeClass("hidden");
+				basketItems.appendChild(listItem);
+				basketCount.innerHTML = basketItems.children.length - 1;
+				price.innerHTML = stuff.innerHTML * priceInitial;
+				minus.classList.remove("disabled");
+				minus.disabled = false;
+				$(".js-scroll").scrollTop($(".js-scroll")[0].scrollHeight);
+
+				if (stuff.innerHTML > 1) {
+					listItemAmount.classList.remove("hidden");
+					listItemAmount.innerHTML = "x" + stuff.innerHTML;
+				}
+
+
+			});
+
+			minus.addEventListener("click", function () {
+				stuff.innerHTML--;
+				listItemAmount.innerHTML = "x" + stuff.innerHTML;
+				price.innerHTML = stuff.innerHTML * priceInitial;
+
+				if (stuff.innerHTML <= 0) {
+					stuff.innerHTML = 0;
+					price.innerHTML = priceInitial;
+					minus.disabled = true;
+					listItemAmount.classList.add("hidden");
+					listItem.parentNode.removeChild(listItem);
+					basketCount.innerHTML = basketItems.children.length - 1;
+					if (basketItems.children.length <= 1) {
+						$(basketItems).parents(".basket__block").addClass("hidden");
+					}
+				}
+			})
+		});
+
+	})();
 });
